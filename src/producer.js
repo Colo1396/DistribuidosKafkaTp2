30bea7b0e9b349651eea8dcc5d833f275b28ba11
Colo1396 <nazarenogalvan@outlook.com>
2021-09-19 11:14:49 -0300

    .

--- src/producer.js
+++ src/producer.js
@@ -6,10 +6,19 @@ const topic = "testTopic"; /** el topic por donde se van a escribir los mensajes
 const kafka = new Kafka({ clientId, brokers }); /** initialize a new kafka client and initialize a producer from it */
 const producer = kafka.producer()
 
+const post =[];
+
 /** esta funcion asincrona va a generar un msg cada 15 segundos **/
  const produce = async () => {
 	await producer.connect();
 	let i = 0
+
+	let newPost ={
+        "topic":"usuario",
+        "titulo":"hola juan carlo",
+        "imagen":"https://store-images.s-microsoft.com/image/apps.18195.14259955503324792.fee0f975-9292-4852-ad19-c6ec880d57d3.35e9f104-1086-48a4-a429-c1c8dd863347?w=180&h=270&q=60",
+        "descripcion":"Hello world"	
+	}
 
 	//inicializo el intervalo de tiempo, 15 seg
 	setInterval(async () => {
@@ -27,6 +36,8 @@ const producer = kafka.producer()
 
 			console.log(`Producer: El mensaje ${i} fue escrito con exito!!!`);
 			i++
+			post.push(newPost)
+			console.log(newPost);
 		} catch (err) {
 			console.error("No se pudo escribir el mensaje debido a --> " + err)
 		}
