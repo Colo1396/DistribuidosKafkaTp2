f0fc8e4b95f6526924281ba5f59528299e785bb8
Colo1396 <nazarenogalvan@outlook.com>
2021-09-20 19:09:28 -0300

    Funcional con cambios de noticias

--- src/app.js
+++ src/app.js
@@ -20,16 +20,38 @@ app.use(express.static(path.join(__dirname, './views/static')));
 //SETTINGS---------------------------------------------
 app.set('json spaces', 2);
 app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
 //ROUTES-----------------------------------------------
 app.get('/', (req, res) => {
     var mensajes = []
-    res.render('home', { mensajes: mensajes });
+    res.render('home.pug', { mensajes: mensajes });
     // llamo a la funcion "consume" , e imprime cualquier error
     consume(io, mensajes).catch((err) => {
         console.error("Error en consumer: ", err)
     })
+});
+
+//ESTE METODO MUESTRA LA NOTICIA EN BASE AL a un msj con formato json
+app.get('/noticiasDesdeCadenaTipoJson', (req, res) => {
+
+    let post =
+    [
+        {
+            "topic": "usuario",
+            "titulo": "hola juan carlo",
+            "imagen": "https://store-images.s-microsoft.com/image/apps.18195.14259955503324792.fee0f975-9292-4852-ad19-c6ec880d57d3.35e9f104-1086-48a4-a429-c1c8dd863347?w=180&h=270&q=60",
+            "descripcion": "Hello world"
+        },
+        {
+            "topic": "usuario",
+            "titulo": "hola juan carlo",
+            "imagen": "https://store-images.s-microsoft.com/image/apps.18195.14259955503324792.fee0f975-9292-4852-ad19-c6ec880d57d3.35e9f104-1086-48a4-a429-c1c8dd863347?w=180&h=270&q=60",
+            "descripcion": "Hello world"
+        }
+    ]
+    res.render('noticias.ejs',{post})
 });
 
 io.on('connection', (socket) => {
--- src/producer.js
+++ src/producer.js
@@ -11,6 +11,14 @@ const producer = kafka.producer()
 	await producer.connect();
 	let i = 0
 
+	//Ejemplo para guardar cuando el productor genera un msj
+	let newPost ={
+        "topic":"usuario",
+        "titulo":"hola juan carlo",
+        "imagen":"https://store-images.s-microsoft.com/image/apps.18195.14259955503324792.fee0f975-9292-4852-ad19-c6ec880d57d3.35e9f104-1086-48a4-a429-c1c8dd863347?w=180&h=270&q=60",
+        "descripcion":"Hello world"	
+	}
+
 	//inicializo el intervalo de tiempo, 15 seg
 	setInterval(async () => {
 		try {
@@ -20,7 +28,7 @@ const producer = kafka.producer()
 				messages: [
 					{
 						key: String(i),
-						value: "Este es el mensaje --> " + i,
+						value: JSON.stringify(newPost),
 					},
 				],
 			})
@@ -30,7 +38,7 @@ const producer = kafka.producer()
 		} catch (err) {
 			console.error("No se pudo escribir el mensaje debido a --> " + err)
 		}
-	}, 15000);
+	}, 5000);
 }
 
 module.exports = produce
