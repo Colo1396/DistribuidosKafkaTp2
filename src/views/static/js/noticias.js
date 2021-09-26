0c82bb9391997b8da687d6304c20eb04d232475a
Colo1396 <nazarenogalvan@outlook.com>
2021-09-26 15:18:48 -0300

    Imprime varias noticias de varios topicos
    Falta pasarle los topicos mediante el req ya que se simulo mediante un json el ingreso de varios topicos

--- src/views/static/js/noticias.js
+++ src/views/static/js/noticias.js
@@ -1,3 +1,5 @@
+//const { response } = require("express");
+
 var topic = document.getElementById("topic");
 var btnConsultar = document.getElementById("consultar");
 
@@ -6,34 +8,36 @@ const api_url = "http://localhost:8080"
 btnConsultar.addEventListener('click', function () {
 
     let post = [];
+    let postAux = [];
+    let listNoticias = [];
 
     let posteos = [
         {
             "topic": "topico1",
             "msg": {
-                        "topic": "post",
-                        "titulo": "Rick y Morty",
-                        "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAYl6MPPo3YndGtPNT6irDc2YVHA6UzXAziw&usqp=CAU",
-                        "descripcion": "Serie"
-                    }
+                "topic": "post",
+                "titulo": "Rick y Morty",
+                "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAYl6MPPo3YndGtPNT6irDc2YVHA6UzXAziw&usqp=CAU",
+                "descripcion": "Serie"
+            }
         },
         {
             "topic": "topico2",
             "msg": {
-                        "topic": "post",
-                        "titulo": "Horizont",
-                        "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcBKU4GfF-8j6SslS7q7c1lanlSbLp47r8pg&usqp=CAU",
-                        "descripcion": "juego"
-                    }
+                "topic": "post",
+                "titulo": "Horizont",
+                "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcBKU4GfF-8j6SslS7q7c1lanlSbLp47r8pg&usqp=CAU",
+                "descripcion": "juego"
+            }
         },
         {
             "topic": "topico3",
             "msg": {
-                        "topic": "post",
-                        "titulo": "HollowKnigh",
-                        "imagen": "https://store-images.s-microsoft.com/image/apps.18195.14259955503324792.fee0f975-9292-4852-ad19-c6ec880d57d3.35e9f104-1086-48a4-a429-c1c8dd863347?w=180&h=270&q=60",
-                        "descripcion": "juego"
-                    }
+                "topic": "post",
+                "titulo": "HollowKnigh",
+                "imagen": "https://store-images.s-microsoft.com/image/apps.18195.14259955503324792.fee0f975-9292-4852-ad19-c6ec880d57d3.35e9f104-1086-48a4-a429-c1c8dd863347?w=180&h=270&q=60",
+                "descripcion": "juego"
+            }
         }
     ];
 
@@ -41,38 +45,38 @@ btnConsultar.addEventListener('click', function () {
         [{
             "topic": "topico1",
             "msg": {
-                        "topic": "post",
-                        "titulo": "Rick y Morty",
-                        "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAYl6MPPo3YndGtPNT6irDc2YVHA6UzXAziw&usqp=CAU",
-                        "descripcion": "Serie"
-                    }
+                "topic": "post",
+                "titulo": "Rick y Morty",
+                "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAYl6MPPo3YndGtPNT6irDc2YVHA6UzXAziw&usqp=CAU",
+                "descripcion": "Serie"
+            }
         }],
         [{
             "topic": "topico2",
             "msg": {
-                        "topic": "post",
-                        "titulo": "Horizont",
-                        "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcBKU4GfF-8j6SslS7q7c1lanlSbLp47r8pg&usqp=CAU",
-                        "descripcion": "juego"
-                    }
+                "topic": "post",
+                "titulo": "Horizont",
+                "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcBKU4GfF-8j6SslS7q7c1lanlSbLp47r8pg&usqp=CAU",
+                "descripcion": "juego"
+            }
         }],
         [{
             "topic": "topico3",
             "msg": {
-                        "topic": "post",
-                        "titulo": "HollowKnigh",
-                        "imagen": "https://store-images.s-microsoft.com/image/apps.18195.14259955503324792.fee0f975-9292-4852-ad19-c6ec880d57d3.35e9f104-1086-48a4-a429-c1c8dd863347?w=180&h=270&q=60",
-                        "descripcion": "juego"
-                    }
+                "topic": "post",
+                "titulo": "HollowKnigh",
+                "imagen": "https://store-images.s-microsoft.com/image/apps.18195.14259955503324792.fee0f975-9292-4852-ad19-c6ec880d57d3.35e9f104-1086-48a4-a429-c1c8dd863347?w=180&h=270&q=60",
+                "descripcion": "juego"
+            }
         },
         {
             "topic": "topico3",
             "msg": {
-                        "topic": "post",
-                        "titulo": "HollowKnigh",
-                        "imagen": "https://store-images.s-microsoft.com/image/apps.18195.14259955503324792.fee0f975-9292-4852-ad19-c6ec880d57d3.35e9f104-1086-48a4-a429-c1c8dd863347?w=180&h=270&q=60",
-                        "descripcion": "juego"
-                    }
+                "topic": "post",
+                "titulo": "HollowKnigh",
+                "imagen": "https://store-images.s-microsoft.com/image/apps.18195.14259955503324792.fee0f975-9292-4852-ad19-c6ec880d57d3.35e9f104-1086-48a4-a429-c1c8dd863347?w=180&h=270&q=60",
+                "descripcion": "juego"
+            }
         }]
     ];
     //console.log("la lista de post es:", post)
@@ -89,7 +93,6 @@ btnConsultar.addEventListener('click', function () {
 
     topicos.forEach(topicos => {
         //console.log("el valor del topico es", topicos.topic)
-
         fetch(api_url + '/traerMensajes', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -99,18 +102,66 @@ btnConsultar.addEventListener('click', function () {
         })
             .then(response => response.json())
             .then(data => {
-                post.push(data);
+                data.forEach(post => {
+                    //console.log("post", post)
+                    //console.log("post.msg", post.msg)
+
+                    let objPOst = {
+                        "topic": post.topic,
+                        "msg": {
+                            "topic": post.msg.topic,
+                            "titulo": post.msg.titulo,
+                            "imagen": post.msg.imagen,
+                            "descripcion": post.msg.descripcion
+                        }
+
+                    }
+                    console.log("objPOst", objPOst)
+                    postAux.push(objPOst)
+                    console.log("postAux", postAux)
+
+
+                    const posteosDiv = document.getElementById('verNoticias');
+                    posteosDiv.innerHTML = ``;
+                    postAux.forEach(posteos => {
+                        const div = document.createElement('div');
+                        div.className = '';
+                        div.innerHTML = `
+                            <div class="col-md-4">
+                                <div class="card bg-dark">
+                                    <div class="card-header text-white d-flex justify-content-between align-items-center">
+                                    ${posteos.msg.topic}/${posteos.msg.titulo}
+                                    </div>
+                                    <img class="card-img-top" src="${posteos.msg.imagen}">
+                                    <div class="card-body text-light">
+                                    ${posteos.msg.descripcion}
+                                    </div>
+                                </div>
+                            </div>
+                            `;
+                        posteosDiv.appendChild(div);
+                    });
+                });
             })
-            .catch(err=>console.log(err));
 
     })
 
     //QUIERO LLENAR LA LISTA CON LOS MSJ DE LOS TOPICOS Y  RECORRERLA PARA IMPRIMR LA IMAGEN
     console.log("la lista de post es:", post)
-    console.log("la lista de posteos es:", posteos)
-    console.log("la lista de posteos2 es:", posteos2)
+    // console.log("la lista de posteos es:", posteos)
+    // console.log("la lista de posteos2 es:", posteos2)
+    console.log("la lista de postAux es:", JSON.stringify(postAux))
 
-    const posteosDiv = document.getElementById('verNoticias');
+    //--------------------------------
+    console.log("la postAux es:", postAux)
+    console.log("la lista de postAux es:", JSON.stringify(postAux))
+    postAux.forEach(datosPOst => {
+        console.log("post  :datosPOst =>", datosPOst)
+    });
+
+
+    //Imprime lista  de posteos como "posteos"
+    /*const posteosDiv = document.getElementById('verNoticias');
     posteosDiv.innerHTML = ``;
     posteos.forEach(posteos => {
         const div = document.createElement('div');
@@ -129,7 +180,39 @@ btnConsultar.addEventListener('click', function () {
         </div>
         `;
         posteosDiv.appendChild(div);
+    });*/
+    //--------------------------------
+
+    //Imprime lista de listas de posteos como "posteos2"
+    /*const posteosDiv = document.getElementById('verNoticias');
+    posteosDiv.innerHTML = ``;
+
+    posteos2.forEach(datos => {
+
+        console.log("posteos 2 :datos =>", datos)
+        datos.forEach(mensaje => {
+            console.log("posteos 2 :datos => mensjae=>", mensaje.msg)
+            const div = document.createElement('div');
+            div.className = '';
+            div.innerHTML = `
+                            <div class="col-md-4">
+                                <div class="card bg-dark">
+                                    <div class="card-header text-white d-flex justify-content-between align-items-center">
+                                    ${mensaje.msg.topic}/${mensaje.msg.titulo}
+                                    </div>
+                                    <img class="card-img-top" src="${mensaje.msg.imagen}">
+                                    <div class="card-body text-light">
+                                    ${mensaje.msg.descripcion}
+                                    </div>
+                                </div>
+                            </div>
+                            `;
+            posteosDiv.appendChild(div);
+
+        });
+
     });
+*/
 
 
 });
